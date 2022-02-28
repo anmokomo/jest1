e5ff6dd9f8aaf4f7d51fdd329cabbee2357e60f9
Anna Koehler <anna@foundrymakes.com>
2022-02-27 20:45:56 -0600

    extra tests

--- filterByTerm.spec.js
+++ filterByTerm.spec.js
@@ -1,3 +1,9 @@
+//function we're testing (putting it in same file for simplicity)
+const filterByTerm = (inputArray, filterTerm) => {
+  if(filterTerm){
+    return inputArray.filter(linkObj => linkObj.url.includes(filterTerm.toLowerCase()))
+  }
+}
 /* describe = Jest function, contains 1+ related tests
 2 args - string description, function callback wrapping the actual test
 */
@@ -9,10 +15,13 @@ describe('filter by term function (link)',() => {
       {id: 2, url: 'www.test157.net'},
       {id: 3, url: 'http://www.mylink.dev'}
     ]
-    const testOutput = [{id: 3, url: 'http://mylink.dev'}]
-
+    const testOutput1 = [{id: 3, url: 'http://www.mylink.dev'}]
+    const testOutput2 = [{id: 1, url: 'https://www.aurl5768.com'}]
     //func usage: filterByTerm(input: Array, term: string)
     //test using inputs and output above:
-    expect(filterByTerm(testInputs, 'link')).toEqual(testOutput)
+    expect(filterByTerm(testInputs, 'link')).toEqual(testOutput1)
+    expect(filterByTerm(testInputs, 'UrL')).toEqual(testOutput2)
+    expect(filterByTerm(testInputs, '')).toEqual(undefined)
+    expect(filterByTerm(testInputs, 'LINK')).toEqual(testOutput1)
   })
 })
