c008e1065942a429bc6b4b80b625ac63ba01ade5
Anna Koehler <anna@foundrymakes.com>
2022-02-27 20:17:00 -0600

    finish filling out first test

--- __tests__/filterByTerm.spec.js
+++ __tests__/filterByTerm.spec.js
@@ -1,8 +1,18 @@
 /* describe = Jest function, contains 1+ related tests
 2 args - string description, function callback wrapping the actual test
 */
-describe('filter by term function',() => {
+describe('filter by term function (link)',() => {
   test('this should filter by search term (url)', () => {
-	//actual test
+    /* create single object(array) for test inputs, and an output object */
+    const testInputs = [
+      {id: 1, url: 'https://www.aurl5768.com'},
+      {id: 2, url: 'www.test157.net'},
+      {id: 3, url: 'http://www.mylink.dev'}
+    ]
+    const testOutput = [{id: 3, url: 'http://mylink.dev'}]
+
+    //func usage: filterByTerm(input: Array, term: string)
+    //test using inputs and output above:
+    expect(filterByTerm(testInputs, 'link')).toEqual(testOutput)
   })
 })
