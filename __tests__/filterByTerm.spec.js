95730c4bdd5819493fffa1427853ff369d92e6b2
Anna Koehler <anna@foundrymakes.com>
2022-02-27 18:51:55 -0600

    add individual test outline

--- __tests__/filterByTerm.spec.js
+++ __tests__/filterByTerm.spec.js
@@ -1,3 +1,8 @@
+/* describe = Jest function, contains 1+ related tests
+2 args - string description, function callback wrapping the actual test
+*/
 describe('filter by term function',() => {
-
+  test('this should filter by search term (url)', () => {
+	//actual test
+  })
 })
